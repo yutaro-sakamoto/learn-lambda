db744b41cb9d56bfc6f05f1c933efa7a59d616f5
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2024-12-30 21:53:12 +0900

    feat: lambda reads data.txt

--- lib/Batch/batch.function.ts
+++ lib/Batch/batch.function.ts
@@ -1,30 +1,37 @@
-import { DynamoDB } from "aws-sdk";
-import { Context, ScheduledEvent } from "aws-lambda";
+//import { DynamoDB } from "aws-sdk";
+import { Context } from "aws-lambda";
+import * as fs from "fs";
 
-const dynamoDb = new DynamoDB.DocumentClient();
-const tableName = process.env.TABLE_NAME || "SampleTable";
+//const dynamoDb = new DynamoDB.DocumentClient();
+//const tableName = process.env.TABLE_NAME || "SampleTable";
+interface Event {
+  length: number;
+}
 
 export const handler = async (
-  event: ScheduledEvent,
+  event: Event,
   context: Context,
 ): Promise<void> => {
   console.log("Event: ", JSON.stringify(event, null, 2));
   console.log("Context: ", JSON.stringify(context, null, 2));
 
-  // Example: Put a new item into the DynamoDB table
-  const params = {
-    TableName: tableName,
-    Item: {
-      id: "example-id",
-      timestamp: new Date().toISOString(),
-      message: "Hello from Lambda!",
-    },
-  };
+  const data = fs.readFileSync("data.txt", "utf8");
+  console.log("Data: ", data);
 
-  try {
-    await dynamoDb.put(params).promise();
-    console.log("Item successfully written to DynamoDB");
-  } catch (error) {
-    console.error("Error writing to DynamoDB", error);
-  }
+  //// Example: Put a new item into the DynamoDB table
+  //const params = {
+  //  TableName: tableName,
+  //  Item: {
+  //    id: "example-id",
+  //    timestamp: new Date().toISOString(),
+  //    message: "Hello from Lambda!",
+  //  },
+  //};
+
+  //try {
+  //  await dynamoDb.put(params).promise();
+  //  console.log("Item successfully written to DynamoDB");
+  //} catch (error) {
+  //  console.error("Error writing to DynamoDB", error);
+  //}
 };
--- lib/Batch/batch.ts
+++ lib/Batch/batch.ts
@@ -1,16 +1,36 @@
 import { Construct } from "constructs";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
-import * as events from "aws-cdk-lib/aws-events";
-import * as targets from "aws-cdk-lib/aws-events-targets";
+//import * as events from "aws-cdk-lib/aws-events";
+//import * as targets from "aws-cdk-lib/aws-events-targets";
 import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 import * as cdk from "@aws-cdk/core";
+import * as lambda from "aws-cdk-lib/aws-lambda";
 
 export class Batch extends Construct {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
     // Create a new Typescript Lambda function
-    const batchFunction = new NodejsFunction(this, "function");
+    const batchFunction = new NodejsFunction(this, "function", {
+      entry: "lib/Batch/batch.function.ts",
+      handler: "handler",
+      runtime: lambda.Runtime.NODEJS_18_X,
+      // Bundle the function with the data.txt file
+      // See https://qiita.com/the_red/items/35d48535924c4ffcbd2d
+      bundling: {
+        commandHooks: {
+          beforeBundling(inputDir: string, outputDir: string): string[] {
+            return [`cp ${inputDir}/lib/Batch/data.txt ${outputDir}`];
+          },
+          afterBundling(): string[] {
+            return [];
+          },
+          beforeInstall(): string[] {
+            return [];
+          },
+        },
+      },
+    });
 
     // Create a new DynamoDB table
     const sampleTable = new dynamodb.Table(this, "SampleTable", {
@@ -21,9 +41,9 @@ export class Batch extends Construct {
     sampleTable.grantReadWriteData(batchFunction);
 
     // Run the Lambda function periodically
-    new events.Rule(this, "Rule", {
-      schedule: events.Schedule.cron({ minute: "0", hour: "4" }),
-      targets: [new targets.LambdaFunction(batchFunction)],
-    });
+    //new events.Rule(this, "Rule", {
+    //  schedule: events.Schedule.cron({ minute: "0", hour: "4" }),
+    //  targets: [new targets.LambdaFunction(batchFunction)],
+    //});
   }
 }
